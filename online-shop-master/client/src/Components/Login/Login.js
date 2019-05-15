f0512c7cec9486de2b5180242275b68ebd146cad
namPS <nam.choi@publicissapient.com>
2019-05-15 16:05:48 -0400

    Login

--- online-shop-master/client/src/Components/Login/Login.js
+++ online-shop-master/client/src/Components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Auth from "../../Auth";
@@ -9,88 +9,81 @@ import TwitterLoginPoc from "./TwitterLoginPoc";
 
 import "./Login.css";
 
-class ConnectedLogin extends Component {
-  state = {
-    userName: "",
-    pass: "",
-    redirectToReferrer: false
-  };
-  render() {
-    const { from } = this.props.location.state || { from: { pathname: "/" } };
+const ConnectedLogin = props => {
+  const [userName, setUserName] = useState("");
+  const [pass, setPass] = useState("");
+  const [redirectToReferrer, setRedirectToReferrer] = useState(false);
+  const [wrongCred, setCred] = useState(false);
 
-    // If user was authenticated, redirect her to where she came from.
-    if (this.state.redirectToReferrer === true) {
-      return <Redirect to={from} />;
-    }
+  const { from } = props.location.state || { from: { pathname: "/" } };
 
-    return (
-      <div className="login-container">
+  // If user was authenticated, redirect her to where she came from.
+  if (redirectToReferrer === true) {
+    return <Redirect to={from} />;
+  }
+
+  return (
+    <div className="login-container">
+      <div
+        style={{
+          height: 300,
+          width: 200,
+          display: "flex",
+          flexDirection: "column"
+        }}
+      >
         <div
           style={{
-            height: 300,
-            width: 200,
-            display: "flex",
-            flexDirection: "column"
+            color: "#504F5A",
+            marginBottom: 50,
+            fontSize: 26,
+            textAlign: "center"
           }}
         >
-          <div
-            style={{
-              color: "#504F5A",
-              marginBottom: 50,
-              fontSize: 26,
-              textAlign: "center"
-            }}
-          >
-            {" "}
-            Log in{" "}
-          </div>
-          <TextField
-            value={this.state.userName}
-            placeholder="User name"
-            onChange={e => {
-              this.setState({ userName: e.target.value });
-            }}
-          />
-          <TextField
-            value={this.state.pass}
-            type="password"
-            placeholder="Password"
-            onChange={e => {
-              this.setState({ pass: e.target.value });
-            }}
-          />
-          <Button
-            style={{ marginTop: 10 }}
-            variant="outlined"
-            color="primary"
-            onClick={() => {
-              // Authenticate the user using entered credentials.
-              Auth.authenticate(this.state.userName, this.state.pass, user => {
-                // Authentication failed.
-                if (!user) {
-                  this.setState({ wrongCred: true });
-                  return;
-                }
-
-                // If we get here, authentication was success.
-                this.props.dispatch(setLoggedInUser({ name: user.name }));
-                this.setState(() => ({
-                  redirectToReferrer: true
-                }));
-              });
-            }}
-          >
-            Log in
-          </Button>
-          {this.state.wrongCred && (
-            <div style={{ color: "red" }}>Wrong username and/or password</div>
-          )}
-          <TwitterLoginPoc />
+          {" "}
+          Log in{" "}
         </div>
+        <TextField
+          value={userName}
+          placeholder="User name"
+          onChange = {(e)=> setUserName(e.target.value)}
+        />
+        <TextField
+          value={pass}
+          type="password"
+          placeholder="Password"
+          onChange={(e)=> setPass(e.target.value)}
+        />
+        <Button
+          style={{ marginTop: 10 }}
+          variant="outlined"
+          color="primary"
+          onClick={() => {
+            // Authenticate the user using entered credentials.
+            Auth.authenticate(userName, pass, user => {
+              // Authentication failed.
+              if (!user) {
+                setCred(true);
+                return;
+              }
+
+              // If we get here, authentication was success.
+              props.dispatch(setLoggedInUser({ name: user.name }));
+              setRedirectToReferrer(true);
+            });
+          }}
+        >
+          Log in
+        </Button>
+        {wrongCred && (
+          <div style={{ color: "red" }}>Wrong username and/or password</div>
+        )}
+        <TwitterLoginPoc />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 const Login = withRouter(connect()(ConnectedLogin));
 
 export default Login;
