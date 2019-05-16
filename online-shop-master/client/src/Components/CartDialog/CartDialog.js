17520bd1b4293d22f60875d4085b50fc2b8a98d4
namPS <nam.choi@publicissapient.com>
2019-05-16 14:42:02 -0400

    CartDialog bug fix

--- online-shop-master/client/src/Components/CartDialog/CartDialog.js
+++ online-shop-master/client/src/Components/CartDialog/CartDialog.js
@@ -28,7 +28,7 @@ const ConnectedCartDialog = props => {
         <Dialog
           open={props.open}
           onClose={() => {
-            this.props.dispatch(showCartDlg(false));
+            props.dispatch(showCartDlg(false));
           }}
         >
           <AppBar position="static" style={{ backgroundColor: "#3863aa" }}>
